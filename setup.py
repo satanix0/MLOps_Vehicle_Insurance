1ba16143f3bc79ff317876d3e656636b08e82450
satanix0 <piyushbxr5@gmail.com>
2025-01-12 11:11:54 +0530

    C1 - Project sturcture Created, venv set

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+"""
+This script is used to set up the packages created within src, int our virtual env. It uses setuptools to define the package metadata and configuration.
+"""
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Piyush",
+    author_email="piyushkr172@gmail.com",
+    packages=find_packages()
+)
---
+++ template.py
@@ -0,0 +1,74 @@
+import os
+from pathlib import Path
+
+"""
+This script sets up the initial project structure for a machine learning operations (MLOps) project related to vehicle insurance.
+It creates a predefined list of directories and files if they do not already exist or are empty.
+Modules:
+    os: Provides a way of using operating system dependent functionality.
+    pathlib: Offers classes representing filesystem paths with semantics appropriate for different operating systems.
+Variables:
+    project_name (str): The name of the main project directory.
+    list_of_files (list): A list of file paths to be created within the project structure.
+Functions:
+    None
+Execution:
+    Iterates through the list_of_files, creates necessary directories, and initializes empty files if they do not already exist or are empty.
+"""
+
+project_name = "src"
+
+list_of_files = [
+
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/configuration/mongo_db_connection.py",
+    f"{project_name}/configuration/aws_connection.py",
+    f"{project_name}/cloud_storage/__init__.py",
+    f"{project_name}/cloud_storage/aws_storage.py",
+    f"{project_name}/data_access/__init__.py",
+    f"{project_name}/data_access/proj1_data.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/entity/estimator.py",
+    f"{project_name}/entity/s3_estimator.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/pipeline/prediction_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    "app.py",
+    "requirements.txt",
+    "Dockerfile",
+    ".dockerignore",
+    "demo.py",
+    "setup.py",
+    "pyproject.toml",
+    "config/model.yaml",
+    "config/schema.yaml",
+]
+
+
+# Iterate through the list of files and create directories
+# and empty files if they do not exist or are empty
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+    else:
+        print(f"file is already present at: {filepath}")
